6b9fbc54cc6bba676397e9004cdf4a209d83bd9c
Developer <dev@example.com>
2026-07-05 11:42:11 +0000

    Initialize project with Vite React scaffold and basic structure

---
+++ src/components/Wizard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Wizard() {
+  return (
+    <div className="wizard">
+      <h1>Story Generator Wizard</h1>
+      <p>Fill out the questionnaire to create your book.</p>
+    </div>
+  );
+}
+
+export default Wizard;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wizard from '../components/Wizard';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <Wizard />
+    </div>
+  );
+}
+
+export default HomePage;
